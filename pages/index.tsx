50ee0bf26d4125df4564283904e6c08fdf14abc1
SLain <sl163@ya.ru>
2021-07-13 13:30:45 +0300

    Добавляет настройки и стартовую (пустую) страницу;

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Baby Interrogatory</title>
+                <meta
+                    name='description'
+                    content='Just interrogatory for parents'
+                />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+            123
+        </div>
+    );
+}
